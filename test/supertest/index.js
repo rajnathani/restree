1808140aab130f41bd4cbc1216ac9591e5c4379b
rajnathani <raj@relfor.com>
2017-10-27 12:33:26 +0530

    fix #1

--- test/supertest/index.js
+++ test/supertest/index.js
@@ -2,12 +2,12 @@ var app = require('express')();
 var restreed = require('./restreed')(app);
 var request = require('supertest');
 
-app.use(function(req, res, next){
-  var metadata = restreed.metadata(req,res);
+app.use(function (req, res, next) {
+  var metadata = restreed.metadata(req, res);
   if (metadata && metadata.runSecurityMiddleware) {
     res.locals.ranSecurityMiddleware = true;
   } else {
-    res.locals.ranSecurityMiddleware = false;    
+    res.locals.ranSecurityMiddleware = false;
   }
   next();
 });
@@ -16,42 +16,55 @@ restreed.bind();
 
 request(app)
   .get('/')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Welcome to groups') throw Error();
   });
 
 request(app)
   .put('/groups')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Created a group') throw Error();
-  });
+  }
+);
 
-  request(app)
+request(app)
   .get('/groups/5cabc387')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Fetched group 5cabc387') throw Error();
-  });
+  }
+);
 
-  request(app)
+request(app)
+  .get('/groups/total')
+  .end(function (err, res) {
+    if (err || res.text !== 'There might be 42 groups') throw Error();
+  }
+);
+
+request(app)
   .delete('/groups/5cabc387')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Deleted group 5cabc387') throw Error();
-  });
+  }
+);
 
-  request(app)
+request(app)
   .post('/groups/5cabc387/members')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Added a new member to group 5cabc387') throw Error();
-  });
+  }
+);
 
-  request(app)
+request(app)
   .post('/groups/5cabc387/members/2uhjg949/suspend')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Suspended member 2uhjg949 from group 5cabc387') throw Error();
-  });
+  }
+);
 
-  request(app)
+request(app)
   .post('/groups/5cabc387/members/2uhjg949/unsuspend')
-  .end(function(err, res){
+  .end(function (err, res) {
     if (err || res.text !== 'Unsuspended member 2uhjg949 from group 5cabc387') throw Error();
-  });
+  }
+);
---
+++ test/supertest/restree/groups/total/GET.js
@@ -0,0 +1,7 @@
+exports.handler = function(req,res) {
+	if (res.locals.ranSecurityMiddleware) {
+		res.status(500, "Metadata Error: Shouldn't have run security middleware");
+	} else {
+		res.status(200).send("There might be 42 groups");
+	}
+};
